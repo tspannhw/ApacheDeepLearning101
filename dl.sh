cbf8e8c5577dbc7bea672ee9f832e167dd1b07db
Timothy Spann <tspann@hortonworks.com>
2018-09-20 16:37:40 -0400

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,7 @@
+curl --header 'Host: data.mxnet.io' --header 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://data.mxnet.io/models/imagenet/' --header 'Connection: keep-alive' 'http://data.mxnet.io/models/imagenet/inception-bn.tar.gz' -o 'inception-bn.tar.gz' -L
+
+tar -xvzf inception-bn.tar.gz
+
+mv Inception_BN-0126.params Inception_BN-0000.params
+
+echo "See https://mxnet.incubator.apache.org/tutorials/embedded/wine_detector.html"
